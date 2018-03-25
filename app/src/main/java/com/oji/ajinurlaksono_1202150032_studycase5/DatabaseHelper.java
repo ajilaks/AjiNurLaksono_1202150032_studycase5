4bb525560715cbbf39ade0ff1f21bd24e63393f9
Aji Nur Laksono <ajilakson@gmail.com>
2018-03-25 21:42:05 +0700

    DB Helper Comments

--- app/src/main/java/com/oji/ajinurlaksono_1202150032_studycase5/DatabaseHelper.java
+++ app/src/main/java/com/oji/ajinurlaksono_1202150032_studycase5/DatabaseHelper.java
@@ -13,7 +13,7 @@ import java.text.DateFormat;
  */
 
 public class DatabaseHelper extends SQLiteOpenHelper {
-
+//membuat variable
     private static final String Table = "todolist";
     private static final String kolom1 = "id";
     private static final String kolom2 = "nama";
@@ -23,7 +23,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public DatabaseHelper(Context context) {
         super(context, Table, null, 1);
     }
-
+//inisiasi create variable
     @Override
     public void onCreate(SQLiteDatabase db) {
         String createSql = "CREATE TABLE " + Table + "(" + kolom1 + " INTEGER PRIMARY KEY AUTOINCREMENT," +
@@ -38,13 +38,13 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         db.execSQL("DROP TABLE IF EXISTS " + Table);
         onCreate(db);
     }
-
+//membuat method add data
     public boolean addData(String nama, String deskripsi, String prioritas) {
-        SQLiteDatabase db = this.getWritableDatabase();
-        ContentValues in = new ContentValues();
-        in.put(kolom2, nama);
-        in.put(kolom3, deskripsi);
-        in.put(kolom4, prioritas);
+        SQLiteDatabase db = this.getWritableDatabase(); //inisiasi
+        ContentValues in = new ContentValues();//instance contentvalues
+        in.put(kolom2, nama); //store data ke database
+        in.put(kolom3, deskripsi);//store data ke database
+        in.put(kolom4, prioritas);//store data ke database
         long result = db.insert(Table, null, in);
         if (result == -1) {
             return false;
@@ -52,17 +52,17 @@ public class DatabaseHelper extends SQLiteOpenHelper {
             return true;
         }
     }
-
+//method get data dari sqlite
     public Cursor getData() {
         SQLiteDatabase db = this.getWritableDatabase();
-        String query = "SELECT * FROM " + Table;
+        String query = "SELECT * FROM " + Table; //query select data
         Cursor data = db.rawQuery(query, null);
         return data;
     }
-
+//method untuk menghapus data
     public void deleteData(int id) {
         SQLiteDatabase db = this.getWritableDatabase();
-        String q = "DELETE FROM " + Table + " WHERE " + kolom1 + " = '" + id + "'";
+        String q = "DELETE FROM " + Table + " WHERE " + kolom1 + " = '" + id + "'"; //query delete data
         db.execSQL(q);
 
     }
