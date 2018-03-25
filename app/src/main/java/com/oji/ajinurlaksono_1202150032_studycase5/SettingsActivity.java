e6eddab97c6185fcaa04142653960b765ae97118
Aji Nur Laksono <ajilakson@gmail.com>
2018-03-25 13:17:04 +0700

    First Commit

---
+++ app/src/main/java/com/oji/ajinurlaksono_1202150032_studycase5/SettingsActivity.java
@@ -0,0 +1,18 @@
+package com.oji.ajinurlaksono_1202150032_studycase5;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SettingsActivity extends AppCompatActivity {
+    public static final String
+            KEY_PREF_EXAMPLE_SWITCH = "example_switch";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        getSupportFragmentManager().beginTransaction()
+                .replace(android.R.id.content, new SettingsFragment())
+                .commit();
+
+    }
+}
---
+++ app/src/main/java/com/oji/ajinurlaksono_1202150032_studycase5/SettingsFragment.java
@@ -0,0 +1,24 @@
+package com.oji.ajinurlaksono_1202150032_studycase5;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.support.v7.preference.PreferenceFragmentCompat;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SettingsFragment extends PreferenceFragmentCompat {
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        setPreferencesFromResource(R.xml.pengaturan, rootKey);
+    }
+
+
+
+}
